9834696766cd1f05f0363b6b55a669cbee79522c
yazkilito@gmail.com <yazkilito@gmail.com>
2025-11-11 18:42:57 +0100

    create json service

---
+++ services/json_service.py
@@ -0,0 +1,17 @@
+
+import json
+import os
+from pathlib import Path
+class jsonService:
+    def __init__(self,json_file_name):
+        self.json_file_path = Path(os.getenv("DATA_FOLDER_PATH")) / json_file_name
+        self.json_file_path.parent.mkdir(parents=True,exist_ok=True)
+
+    
+    def write(self,content):
+        with self.json_file_path.open("w",encoding=os.getenv('ENCODING')) as f:
+            try:
+                json.dump(content,f, ensure_ascii=False, indent=2)
+            except Exception as e:
+                print(f"Error while writing in json file :{e} ")
+
