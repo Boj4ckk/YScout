bf5bcf7c16060a6d0676e9e83c31208687891231
yazkilito@gmail.com <yazkilito@gmail.com>
2025-11-11 18:42:30 +0100

    update main

--- run.py
+++ run.py
@@ -1,21 +1,31 @@
-
+import base64
+import json
 import os
+from pathlib import Path
 
 import google_auth_oauthlib.flow
 import googleapiclient.discovery
 import googleapiclient.errors
-
+import requests
 from dotenv import load_dotenv
 
+
 from services.youtube_service import YoutubeService
+from utils.youtube_utils import YoutubeUtils
+
+from services.json_service import jsonService
+
+
+
+
 
 
 load_dotenv()
 def main():
    
-
+    """
     yt_service = YoutubeService()
-    youtube_client = yt_service.youtube_client()
+    youtube_client = yt_service.youtube_client
 
     request = youtube_client.channels().list(
         part="snippet,contentDetails,statistics",
@@ -23,6 +33,18 @@ def main():
     )
     response = request.execute()
     print(response)
+    """
+    suggestion_list = YoutubeUtils.get_youtube_suggestions_by_keywords()
+    json_service = jsonService(os.getenv("JSON_FILE_NAME"))
+
+    json_service.write(suggestion_list)
+
+
+
+#class="VfPpkd-LgbsSe VfPpkd-LgbsSe-OWXEXe-k8QpJ VfPpkd-LgbsSe-OWXEXe-dgl2Hf nCP5yc AjY5Oe DuMIQc LQeN7 BqKGqe Jskylb TrZEUc lw1w4b"
+    
+
+
 
    
 
