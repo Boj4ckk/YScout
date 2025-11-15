6c4ebe0da35594832694e494555f9cc87a8c6e52
yazkilito@gmail.com <yazkilito@gmail.com>
2025-11-15 19:28:58 +0100

    create insert method for supabase service

--- services/supabase_service.py
+++ services/supabase_service.py
@@ -9,6 +9,18 @@ class SupaBase:
         self.key = os.getenv("SUPABASE_KEY")
 
         try:
-            self.client = create_client(self.url, self.key)
+            self.supabase = create_client(self.url, self.key)
         except Exception as e:
             logger.error(f"Failed to initiate Supabase DB - Make sure to have the right api_key in .env. :{e}")
+    
+
+    def insert_data(self, table, data):
+        try:
+            response = (
+                self.supabase.table(table)
+                .insert(data)
+                .execute()
+            )
+            logging.info(f"Inserted into supabase : {response}")
+        except Exception as e:
+            logger.error(f"Failed to insert data into {table} for the given data: {data}. {e}")
